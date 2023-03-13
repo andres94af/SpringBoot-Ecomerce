cfd6146c799f8a856e996a64a5460520f5110554
Andres Fernandez <andres94.af@gmail.con>
2023-03-13 10:54:33 +0100

    creado repository, service y controller para producto y vista show

---
+++ src/main/java/com/curso/ecomerce/service/ProductoServiceImpl.java
@@ -0,0 +1,37 @@
+package com.curso.ecomerce.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.curso.ecomerce.model.Producto;
+import com.curso.ecomerce.repository.ProductoRespository;
+
+@Service
+public class ProductoServiceImpl implements ProductoService {
+
+	@Autowired
+	private ProductoRespository repository;
+
+	@Override
+	public Producto save(Producto producto) {
+		return repository.save(producto);
+	}
+
+	@Override
+	public Optional<Producto> get(Integer id) {
+		return repository.findById(id);
+	}
+
+	@Override
+	public void update(Producto producto) {
+		repository.save(producto);
+	}
+
+	@Override
+	public void delete(Integer id) {
+		repository.deleteById(id);
+	}
+
+}
