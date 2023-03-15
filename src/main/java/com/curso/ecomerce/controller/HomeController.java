f5248e632bf1ad07167bfaa9f46d582ecf619700
Andres Fernandez <andres94.af@gmail.con>
2023-03-15 11:30:42 +0100

    creado el home controller, vista detallada de producto en producto home y template th para usuarios con redireccion a la home

--- src/main/java/com/curso/ecomerce/controller/HomeController.java
+++ src/main/java/com/curso/ecomerce/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.curso.ecomerce.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecomerce.model.Producto;
@@ -14,6 +17,8 @@ import com.curso.ecomerce.service.ProductoService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	Logger LOGGER = LoggerFactory.getLogger(getClass());
 
 	@Autowired
 	private ProductoService productoService;
@@ -23,6 +28,15 @@ public class HomeController {
 		List<Producto> productos = productoService.findAll();
 		model.addAttribute("productos", productos);
 		return "usuario/home";
+	}
+	
+	@GetMapping("/productohome/{id}")
+	public String productoHome(Model model,@PathVariable Integer id) {
+		Optional<Producto> producto = productoService.get(id);
+		Producto p = producto.get();
+		LOGGER.info("Este es el producto recibido {}", p);
+		model.addAttribute("producto", p);
+		return "usuario/productohome";
 	}
 
 }
