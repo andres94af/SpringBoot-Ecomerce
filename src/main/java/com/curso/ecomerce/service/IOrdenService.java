8720c1df5e11ee70396d8e148899e31b19d6148e
Andres Fernandez <andres94.af@gmail.con>
2023-03-17 23:39:00 +0100

    metodo para generar el numero de la orden con ceros adelante

--- src/main/java/com/curso/ecomerce/service/IOrdenService.java
+++ src/main/java/com/curso/ecomerce/service/IOrdenService.java
@@ -1,7 +1,10 @@
 package com.curso.ecomerce.service;
 
+import java.util.List;
+
 import com.curso.ecomerce.model.Orden;
 
 public interface IOrdenService {
+	List<Orden> findAll();
 	Orden save (Orden orden);
 }
--- src/main/java/com/curso/ecomerce/service/OrdenServiceImpl.java
+++ src/main/java/com/curso/ecomerce/service/OrdenServiceImpl.java
@@ -1,5 +1,8 @@
 package com.curso.ecomerce.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,42 @@ public class OrdenServiceImpl implements IOrdenService {
 	@Override
 	public Orden save(Orden orden) {
 		return ordenRepository.save(orden);
+	}
+
+	@Override
+	public List<Orden> findAll() {
+		return ordenRepository.findAll();
+	}
+	
+	public String generarNumeroOrden() {
+		int numero = 0;
+		String numeroConcatenado = "";
+		//crea lista de todas las ordenes
+		List<Orden> ordenes = findAll();
+		//Crea una lista para almacenar los numeros como Integer porque de la tabla vienen como string
+		List<Integer> numeros = new ArrayList<Integer>();
+		//por cada una de las ordenes parsea de string a integer y lo agrega a la lista de numeros
+		ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+		//Condicional que obtiene el numero de orden mas grande, lo asigna a la variable numero y le suma 1
+		if (ordenes.isEmpty()) {
+			numero = 1;
+		}else {
+			numero = numeros.stream().max(Integer::compare).get();
+			numero++;
+		}
+		
+		if (numero<10) {
+			numeroConcatenado = "0000"+String.valueOf(numero);
+		}else if(numero<100) {
+			numeroConcatenado = "000"+String.valueOf(numero);
+		}else if(numero<1000) {
+			numeroConcatenado = "00"+String.valueOf(numero);
+		}else if(numero<10000) {
+			numeroConcatenado = "0"+String.valueOf(numero);
+		}
+		
+		
+		return numeroConcatenado;
 	}
 
 }
