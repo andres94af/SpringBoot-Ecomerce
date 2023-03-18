102df9747ceb2cbec611797b142d9676a9340b07
Andres Fernandez <andres94.af@gmail.con>
2023-03-18 10:40:49 +0100

    obtener usuario login y session

--- src/main/java/com/curso/ecomerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecomerce/controller/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.curso.ecomerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,27 +13,53 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.curso.ecomerce.model.Usuario;
 import com.curso.ecomerce.service.IUsuarioService;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
-	
+
 	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
 	@Autowired
 	private IUsuarioService usuarioService;
-	
+
 	@GetMapping("/registro")
 	public String create() {
 		return "usuario/registro";
 	}
-	
+
 	@PostMapping("/save")
 	public String saveUser(Usuario usuario) {
 		LOGGER.info("Usuario registrado: {}", usuario);
 		usuario.setTipo("USER");
 		usuarioService.save(usuario);
-		
 		return "redirect:/";
 	}
-	
+
+	@GetMapping("/login")
+	public String login() {
+		return "usuario/login";
+	}
+
+	@PostMapping("/acceder")
+	public String acceder(Usuario usuario, HttpSession session) {
+		LOGGER.info("Accesos: {}", usuario);
+
+		Optional<Usuario> user = usuarioService.findByEmail(usuario.getEmail());
+
+		if (user.isPresent()) {
+			session.setAttribute("idusuario", user.get().getId());
+			if (user.get().getTipo().equals("ADMIN")) {
+				return "redirect:/administrador";
+			} else {
+				return "redirect:/";
+			}
+		} else {
+			LOGGER.info("Usuario no existe");
+		}
+
+		return "redirect:/";
+	}
+
 }
--- src/main/java/com/curso/ecomerce/service/UsuarioServiceImpl.java
+++ src/main/java/com/curso/ecomerce/service/UsuarioServiceImpl.java
@@ -24,6 +24,11 @@ public class UsuarioServiceImpl implements IUsuarioService {
 		return usuarioRepository.save(usuario);
 	}
 
+	@Override
+	public Optional<Usuario> findByEmail(String email) {
+		return usuarioRepository.findByEmail(email);
+	}
+
 
 
 }
