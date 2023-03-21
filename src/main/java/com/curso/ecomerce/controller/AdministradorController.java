2a23a22deddfe3327f6e0021c1b581904b65cec0
Andres Fernandez <andres94.af@gmail.con>
2023-03-21 13:29:51 +0100

    funcionalidad para ver lista de usuarios siendo administrador

--- src/main/java/com/curso/ecomerce/controller/AdministradorController.java
+++ src/main/java/com/curso/ecomerce/controller/AdministradorController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecomerce.model.Producto;
+import com.curso.ecomerce.model.Usuario;
+import com.curso.ecomerce.repository.IUsuarioRepository;
+import com.curso.ecomerce.service.IUsuarioService;
 import com.curso.ecomerce.service.ProductoService;
 
 @Controller
@@ -18,13 +21,20 @@ public class AdministradorController {
 	@Autowired
 	private ProductoService productoService;
 	
+	@Autowired
+	private IUsuarioService usuarioService;
+	
 	@GetMapping("")
 	public String home(Model model) {
-		
 		List<Producto> productos = productoService.findAll();
 		model.addAttribute("productos", productos);
-		
 		return "administrador/home";
+	}
+	
+	@GetMapping("/usuarios")
+	public String usuarios(Model model) {
+		model.addAttribute("usuarios", usuarioService.findAll());
+		return "administrador/usuarios";
 	}
 
 }
