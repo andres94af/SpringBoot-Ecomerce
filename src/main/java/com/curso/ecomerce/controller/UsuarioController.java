58c4571ac7bd758fcd6b6248442000fa401c71b3
Andres Fernandez <andres94.af@gmail.con>
2023-03-21 13:04:34 +0100

    funcionalidad para ver detalle de compra al usuario

--- src/main/java/com/curso/ecomerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecomerce/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -76,6 +77,15 @@ public class UsuarioController {
 		model.addAttribute("sesion", session.getAttribute("idusuario"));
 		model.addAttribute("ordenes", ordenes);
 		return "usuario/compras";
+	}
+	
+	@GetMapping("/detalle/{id}")
+	public String detalleCompra(@PathVariable Integer id, HttpSession session, Model model) {
+		LOGGER.info("id de la orden: {}", id);
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
+		Orden orden = ordenService.findById(id).get();
+		model.addAttribute("detalles", orden.getDetalle());
+		return "usuario/detallecompra";
 	}
 
 }
